39bf95128634391c4827676113f472dd391b88fc
Mickael Royer <mickael.royer@enac.fr>
2018-04-29 23:02:26 +0200

    Prepare new release

--- pynetem/__init__.py
+++ pynetem/__init__.py
@@ -15,7 +15,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-__version__ = "0.4.2"
+__version__ = "0.6.0"
 NETEM_ID = "netem"
 
 
