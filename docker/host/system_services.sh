ab117f4d3f095193d2e3d802e1153b553c82b0cd
Mickael Royer <mickael.royer@enac.fr>
2018-09-17 18:38:24 +0200

    Update docker host

--- docker/host/system_services.sh
+++ docker/host/system_services.sh
@@ -11,3 +11,10 @@ chmod +x /usr/bin/iinit.sh
 $minimal_apt_get_install python3 python3-pyroute2
 cp /build/network-config.py /usr/bin/
 chmod +x /usr/bin/network-config.py
+
+## install multicast programs
+$minimal_apt_get_install build-essential
+gcc -o /tmp/mult_receive /build/programs/mult_receive.c
+gcc -o /tmp/mult_send /build/programs/mult_send.c
+mv /tmp/mult_receive /usr/bin/
+mv /tmp/mult_send /usr/bin/
