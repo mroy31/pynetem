51146dc7920fc503c77e57a7545c7ea049965497
Mickael Royer <mickael.royer@enac.fr>
2018-09-23 15:16:29 +0200

    Update the server to clean up switch/host before running

--- pynetem/daemon/server.py
+++ pynetem/daemon/server.py
@@ -22,9 +22,8 @@ from socketserver import UnixStreamServer, BaseRequestHandler
 import subprocess
 import logging
 import shlex
-from pynetem import __version__
+from pynetem import __version__, NETEM_ID
 from pynetem import NetemError
-from pynetem.ui.config import NetemConfig
 from pynetem.utils import get_exc_desc
 from pynetem.utils import cmd_call
 
@@ -58,11 +57,25 @@ CMD_LIST = {
 }
 
 
-class NetemDaemonHandler(BaseRequestHandler):
+def run_command(cmd_line, check_output=False, shell=False):
+    args = shlex.split(cmd_line)
+    if check_output:
+        try:
+            result = subprocess.check_output(args, shell=shell)
+        except subprocess.CalledProcessError:
+            msg = "Unable to execute command %s" % (cmd_line,)
+            logging.error(msg)
+            raise NetemError(msg)
+        return result.decode("utf-8").strip("\n")
+    else:
+        ret = subprocess.call(args, shell=shell)
+        if ret != 0:
+            msg = "Unable to excecute command %s" % (cmd_line,)
+            logging.error(msg)
+            raise NetemError(msg)
 
-    def setup(self):
-        BaseRequestHandler.setup(self)
-        self.config = NetemConfig()
+
+class NetemDaemonHandler(BaseRequestHandler):
 
     def handle(self):
         cmd = self.request.recv(1024).decode("utf-8").strip()
@@ -96,49 +109,59 @@ class NetemDaemonHandler(BaseRequestHandler):
                 msg += " %s" % ret
         self.request.sendall(msg.encode("utf-8"))
 
-    def version(self):
+    @staticmethod
+    def version():
         return __version__
 
-    def docker_create(self, name, container_name, image):
+    @staticmethod
+    def docker_create(name, container_name, image):
         logging.debug("Create docker container %s" % container_name)
-        self.__command("docker create --privileged --cap-add=ALL --net=none "
-                       "-h %s --name %s %s" % (name, container_name, image))
+        run_command("docker create --privileged --cap-add=ALL --net=none "
+                    "-h %s --name %s %s" % (name, container_name, image))
 
-    def docker_attach_interface(self, container_name, if_name, target_name):
+    @staticmethod
+    def docker_attach_interface(container_name, if_name, target_name):
         logging.debug("Docker : attach if %s to container "
                       "%s" % (if_name, container_name))
-        self.__command("docker exec %s ip link set %s "
-                       "name %s" % (container_name, if_name, target_name))
-        self.__command("docker exec %s ip link set %s "
-                       "up" % (container_name, target_name))
+        run_command("docker exec %s ip link set %s "
+                    "name %s" % (container_name, if_name, target_name))
+        run_command("docker exec %s ip link set %s "
+                    "up" % (container_name, target_name))
 
-    def docker_start(self, container_name):
+    @staticmethod
+    def docker_start(container_name):
         logging.debug("Start docker container %s" % container_name)
-        self.__command("docker start %s" % container_name)
+        run_command("docker start %s" % container_name)
 
-    def docker_stop(self, container_name):
+    @staticmethod
+    def docker_stop(container_name):
         logging.debug("Stop docker container %s" % container_name)
-        self.__command("docker stop %s" % container_name)
+        run_command("docker stop %s" % container_name)
 
-    def docker_rm(self, container_name):
+    @staticmethod
+    def docker_rm(container_name):
         logging.debug("Delete docker container %s" % container_name)
-        self.__command("docker rm %s" % container_name)
+        run_command("docker rm %s" % container_name)
 
-    def docker_pid(self, container_name):
+    @staticmethod
+    def docker_pid(container_name):
         logging.debug("Get PID of docker container %s" % container_name)
-        pid = self.__command("docker inspect --format '{{.State.Pid}}' "
-                             "%s" % container_name, check_output=True)
+        pid = run_command("docker inspect --format '{{.State.Pid}}' "
+                          "%s" % container_name, check_output=True)
         return pid
 
-    def docker_cp(self, source, dest):
+    @staticmethod
+    def docker_cp(source, dest):
         logging.debug("Docker cp from %s to %s" % (source, dest))
-        self.__command("docker cp %s %s" % (source, dest))
+        run_command("docker cp %s %s" % (source, dest))
 
-    def docker_exec(self, container_name, cmd_line):
+    @staticmethod
+    def docker_exec(container_name, cmd_line):
         logging.debug("Docker %s : exec %s" % (container_name, cmd_line))
-        self.__command("docker exec %s %s" % (container_name, cmd_line))
+        run_command("docker exec %s %s" % (container_name, cmd_line))
 
-    def docker_shell(self, c_name, name, shell, display, xauth, term_cmd):
+    @classmethod
+    def docker_shell(cls, c_name, name, shell, display, xauth, term_cmd):
         logging.debug("Docker open shell for container %s" % c_name)
         term_cmd = term_cmd % {
             "title": name,
@@ -147,27 +170,31 @@ class NetemDaemonHandler(BaseRequestHandler):
         args = shlex.split(term_cmd)
         subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
                          stderr=subprocess.PIPE, shell=False,
-                         env=self.__set_x11_env(display, xauth))
+                         env=cls.__set_x11_env(display, xauth))
 
-    def docker_capture(self, display, xauth, c_name, if_name):
+    @classmethod
+    def docker_capture(cls, display, xauth, c_name, if_name):
         logging.debug("Docker launch capture on if %s:%s" % (c_name, if_name))
         pretty_name = c_name.split(".", 1)[1]
         cmd = "/bin/bash -c 'docker exec {0} tcpdump -s 0 -U -w - -i {1} "\
               "2>/dev/null | wireshark -o 'gui.window_title:{1}@{2}' "\
               "-k -i - &'".format(c_name, if_name, pretty_name)
         subprocess.call(shlex.split(cmd),
-                        env=self.__set_x11_env(display, xauth))
+                        env=cls.__set_x11_env(display, xauth))
 
-    def tap_create(self, name, user):
+    @staticmethod
+    def tap_create(name, user):
         logging.debug("Create tap %s" % name)
-        self.__command("tunctl -u %s -t %s" % (user, name))
-        self.__command("ip link set %s up" % (name,))
+        run_command("tunctl -u %s -t %s" % (user, name))
+        run_command("ip link set %s up" % (name,))
 
-    def tap_delete(self, name):
+    @staticmethod
+    def tap_delete(name):
         logging.debug("Delete tap %s" % name)
-        self.__command("tunctl -d %s" % (name,))
+        run_command("tunctl -d %s" % (name,))
 
-    def __is_pid(self, pid):
+    @staticmethod
+    def __is_pid(pid):
         try:
             int(pid)
         except ValueError:
@@ -175,103 +202,100 @@ class NetemDaemonHandler(BaseRequestHandler):
         else:
             return True
 
-    def netns_create(self, name):
+    @classmethod
+    def netns_create(cls, name):
         logging.debug("Create netns %s" % name)
-        if not self.__is_pid(name):
-            self.__command("ip netns add %s" % name)
+        if not cls.__is_pid(name):
+            run_command("ip netns add %s" % name)
         else:
             if not os.path.isdir("/var/run/netns"):
                 os.mkdir("/var/run/netns")
-            self.__command("ln -s /proc/%s/ns/net "
-                           "/var/run/netns/%s" % (name, name))
+            run_command("ln -s /proc/%s/ns/net "
+                        "/var/run/netns/%s" % (name, name))
 
-    def netns_delete(self, name):
+    @classmethod
+    def netns_delete(cls, name):
         logging.debug("Delete netns %s" % name)
-        if not self.__is_pid(name):
-            self.__command("ip netns del %s" % name)
+        if not cls.__is_pid(name):
+            run_command("ip netns del %s" % name)
         else:
-            self.__command("rm /var/run/netns/%s" % name)
+            run_command("rm /var/run/netns/%s" % name)
 
-    def link_create(self, if1, if2):
+    @staticmethod
+    def link_create(if1, if2):
         logging.debug("Create link %s<-->%s" % (if1, if2))
-        self.__command("ip link add %s type veth peer name %s" % (if1, if2))
+        run_command("ip link add %s type veth peer name %s" % (if1, if2))
 
-    def link_delete(self, if_name):
+    @staticmethod
+    def link_delete(if_name):
         logging.debug("Delete link %s" % if_name)
-        self.__command("ip link del %s" % if_name)
+        run_command("ip link del %s" % if_name)
 
-    def link_netns(self, if_name, netns):
+    @staticmethod
+    def link_netns(if_name, netns):
         logging.debug("Attach link %s to namespace %s" % (if_name, netns))
-        self.__command("ip link set %s netns %s" % (if_name, netns))
+        run_command("ip link set %s netns %s" % (if_name, netns))
 
-    def link_set_vtap(self, if_name, netns):
+    @staticmethod
+    def link_set_vtap(if_name, netns):
         logging.debug("Set link %s of namespace %s macvtap" % (if_name, netns))
-        self.__command("ip netns exec %s ip link add link %s "
-                       "type macvtap mode vepa" % (netns, if_name))
-        self.__command("ip netns exec %s ip link set macvtap0 up" % netns)
+        run_command("ip netns exec %s ip link add link %s "
+                    "type macvtap mode vepa" % (netns, if_name))
+        run_command("ip netns exec %s ip link set macvtap0 up" % netns)
 
-    def __is_ovsbr_exist(self, sw_name):
+    @staticmethod
+    def __is_ovsbr_exist(sw_name):
         return cmd_call("ovs-vsctl br-exists %s" % sw_name) != 2
 
-    def ovs_create(self, sw_name):
+    @classmethod
+    def ovs_create(cls, sw_name):
         logging.debug("Create switch %s" % sw_name)
-        if self.__is_ovsbr_exist(sw_name):
+        if cls.__is_ovsbr_exist(sw_name):
             return "EXIST"
-        self.__command("ovs-vsctl add-br %s" % sw_name)
+        run_command("ovs-vsctl add-br %s" % sw_name)
 
-    def ovs_delete(self, sw_name):
+    @classmethod
+    def ovs_delete(cls, sw_name):
         logging.debug("Delete switch %s" % sw_name)
-        if self.__is_ovsbr_exist(sw_name):
-            self.__command("ovs-vsctl del-br %s" % sw_name)
+        if cls.__is_ovsbr_exist(sw_name):
+            run_command("ovs-vsctl del-br %s" % sw_name)
 
-    def ovs_add_port(self, sw_name, p_name):
+    @staticmethod
+    def ovs_add_port(sw_name, p_name):
         logging.debug("Add port %s to switch %s" % (p_name, sw_name))
-        self.__command("ovs-vsctl add-port %s %s" % (sw_name, p_name))
-        self.__command("ip link set %s up" % p_name)
+        run_command("ovs-vsctl add-port %s %s" % (sw_name, p_name))
+        run_command("ip link set %s up" % p_name)
 
-    def ovs_port_vlan(self, p_name, vlan):
+    @staticmethod
+    def ovs_port_vlan(p_name, vlan):
         logging.debug("Set port %s to belong to vlan %s" % (p_name, vlan))
-        self.__command("ovs-vsctl set port %s tag=%s" % (p_name, vlan))
+        run_command("ovs-vsctl set port %s tag=%s" % (p_name, vlan))
 
-    def ovs_del_port(self, sw_name, p_name):
+    @staticmethod
+    def ovs_del_port(sw_name, p_name):
         logging.debug("Delete port %s from switch %s" % (p_name, sw_name))
-        self.__command("ovs-vsctl del-port %s %s" % (sw_name, p_name))
+        run_command("ovs-vsctl del-port %s %s" % (sw_name, p_name))
 
-    def clean(self, prj_id):
+    @classmethod
+    def clean(cls, prj_id):
         logging.debug("Clean project %s" % prj_id)
         # remove existing docker containers
         d_cmd = "docker container list --format '{{.Names}}' --all"
-        containers = self.__command(d_cmd, check_output=True).split("\n")
+        containers = run_command(d_cmd, check_output=True).split("\n")
         for container_name in containers:
             if container_name.startswith(prj_id):
-                self.docker_stop(container_name)
-                self.docker_rm(container_name)
+                cls.docker_stop(container_name)
+                cls.docker_rm(container_name)
 
         # remove existing ovs switches
         s_cmd = "ovs-vsctl list-br"
-        switches = self.__command(s_cmd, check_output=True).split("\n")
+        switches = run_command(s_cmd, check_output=True).split("\n")
         for s_name in switches:
             if s_name.startswith(prj_id):
-                self.ovs_delete(s_name)
+                cls.ovs_delete(s_name)
 
-    def __command(self, cmd_line, check_output=False, shell=False):
-        args = shlex.split(cmd_line)
-        if check_output:
-            try:
-                result = subprocess.check_output(args, shell=shell)
-            except subprocess.CalledProcessError:
-                msg = "Unable to execute command %s" % (cmd_line,)
-                logging.error(msg)
-                raise NetemError(msg)
-            return result.decode("utf-8").strip("\n")
-        else:
-            ret = subprocess.call(args, shell=shell)
-            if ret != 0:
-                msg = "Unable to excecute command %s" % (cmd_line,)
-                logging.error(msg)
-                raise NetemError(msg)
-
-    def __set_x11_env(self, display, xauth):
+    @staticmethod
+    def __set_x11_env(display, xauth):
         env = {"DISPLAY": display}
         if xauth != "null":
             env["XAUTHORITY"] = xauth
@@ -288,6 +312,9 @@ class NetemDaemonThread(threading.Thread):
         self.running = False
 
     def run(self):
+        # before starting the server, clean the env
+        NetemDaemonHandler.clean(NETEM_ID)
+
         self.running = True
         self.__server = UnixStreamServer(self.__socket, NetemDaemonHandler)
         os.chmod(self.__socket, 0o666)
