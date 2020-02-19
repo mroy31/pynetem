f0f8cc1d0f76ed8f05b022ab151169a2b4cf86f3
Mickael Royer <mickael.royer@enac.fr>
2020-02-19 16:20:17 +0100

    [docker] modify host image to have a dhcp/tftp client

--- docker/host/utilities.sh
+++ docker/host/utilities.sh
@@ -6,4 +6,4 @@ set -x
 ## install tools
 $minimal_apt_get_install curl less nano vim psmisc tcpdump iputils-ping \
 iputils-arping iputils-tracepath net-tools file telnet man traceroute hping3 \
-iptables links lighttpd htop
+iptables links lighttpd htop tftp-hpa isc-dhcp-client
