50634f12c5e76ea1d0802f8e0fcc0183a08f0845
roy <roy@weiss.recherche.enac.fr>
2020-04-28 22:39:12 +0200

    [docker] simplify docker host image

--- docker/host/utilities.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-source /build/buildconfig
-set -x
-
-## install tools
-$minimal_apt_get_install curl less nano vim psmisc tcpdump iputils-ping \
-iputils-arping iputils-tracepath net-tools file telnet man traceroute hping3 \
-iptables links lighttpd htop tftp-hpa isc-dhcp-client
