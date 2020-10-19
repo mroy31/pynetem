981a78298a2e195204f62f94bcb45c197ea56e4e
roy <roy@weiss.recherche.enac.fr>
2020-10-19 10:02:19 +0200

    Bump version number for new release

--- pynetem/__init__.py
+++ pynetem/__init__.py
@@ -15,7 +15,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-__version__ = "0.13.1"
+__version__ = "0.14.0"
 NETEM_ID = "ntm"
 
 
